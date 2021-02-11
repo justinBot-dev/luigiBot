e3fcdbb13a869ca041c6c8776174a6c8ea95b85a
justinBot-dev <78810818+justinBot-dev@users.noreply.github.com>
2021-02-11 15:52:10 -0500

    Add files via upload
    luigi discord bot YOU NEED TO DOWNLOAD ALL THE DISCORD BOT STUFF because I'm too lazy and cant put above 100 files

---
+++ luigi.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+client.on('ready', () =>{
+    console.log('Hail Luigi!!! >:)')
+    client.user.setActivity('luigi is peace')
+});
+
+client.on("message", (m) => {
+    
+    if(m.content != "luigi"){
+        if(!m.member.roles.cache.some(r => r.name === "luigi bypass")){
+            if(m.channel.name === "speak-the-answer"){
+                m.author.send('You need to hail luigi, luigi is peace. https://discord.gg/YpgXeFZGQU').catch(err => console.log(`Couldnt send message to ${m.author.username}`))
+                m.delete().catch(err => console.log('Couldnt delete message'));
+                m.member.kick().catch(err => console.log('Can not kick member'));
+            }
+        }
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login("ODA5NDc4OTE1MTg5ODk5MzE0.YCVr_A.PS8MLSFBJs8rTzhBQQ6B-reo0tw");
